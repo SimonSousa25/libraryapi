d8342697b8a2a12b20a23f4db1b2306815e9d669
SimonSousa25 <simonoliveirasousa02@gmail.com>
2026-06-27 23:57:05 -0300

    Mapeando entidades JPA

---
+++ src/main/java/io/github/springbootproject/libraryapi/model/Autor.java
@@ -0,0 +1,39 @@
+package io.github.springbootproject.libraryapi.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Table(name = "autor", schema = "public")
+@Getter @Setter // annotations que fazem com que os Getters e Setters sejam gerados em tempo de compilação
+public class Autor {
+
+    @Id
+    @Column(name = "id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "nome", length = 100, nullable = false)
+    private String nome;
+
+    @Column(name = "data_nascimento", nullable = false)
+    private LocalDate dataNascimento;
+
+    @Column(name = "nacionalidade", length = 50, nullable = false)
+    private String nacionalidade;
+
+//    @Deprecated
+//    public Autor() {
+//         para uso do framework
+//    }
+//
+//    public Autor(String nome, LocalDate dataNascimento, String nacionalidade) {
+//        this.nome = nome;
+//        this.dataNascimento = dataNascimento;
+//        this.nacionalidade = nacionalidade;
+//    }
+}
